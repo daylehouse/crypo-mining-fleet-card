9e0d999569b0bc27ac0099fbe414634b1d99cdf6
daylehouse <dayle@daylehouse.co.uk>
2026-05-11 11:44:04 +0100

    Layout

--- src/crypto-miner-card.ts
+++ src/crypto-miner-card.ts
@@ -49,7 +49,7 @@ export class CryptoMinerCard extends LitElement {
         width: 100%;
         aspect-ratio: 3 / 4;
         background-size: cover;
-        background-position: center center;
+        background-position: center top;
         background-repeat: no-repeat;
         background-color: #000;
       }
